1d5b7bf36c81b652838d22065327164d6ba4a918
Marcos Artine <marcos.14.41@hotmail.com>
2026-05-01 18:42:04 -0300

    Implementación de la logica de cambio de vista del SocioActivity

--- app/src/main/java/com/grupo4/clubdeportivo/SocioActivity.kt
+++ app/src/main/java/com/grupo4/clubdeportivo/SocioActivity.kt
@@ -1,16 +1,29 @@
 package com.grupo4.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SocioActivity : AppCompatActivity() {
+
+   private lateinit var btnNuevoSocio : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_socio)
+
+        btnNuevoSocio = findViewById(R.id.btnNuevoSocio)
+
+
+        btnNuevoSocio.setOnClickListener {
+            val aNuevoSocio = Intent(this, NuevoSocioActivity::class.java)
+            startActivity(aNuevoSocio)
+        }
 
     }
 }
